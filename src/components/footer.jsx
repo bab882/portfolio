570e645986e07736532ef0a1f7d738ec5570f9a7
bab882 <chanitebrandon@gmail.com>
2024-09-19 15:27:13 +0200

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -15,7 +15,33 @@ function Footer(){
                 <div className="footer__title">
                     <h2 className="subtitle">Interested in talking, let’s do it.</h2>
                     <a className="btn btn1 justify-content-center mx-2" href="mailto: chanitebrandon@gmail.com"  type="button">Contact</a>
-                    <div className="line2"></div>
+                    <div className="line2 ">
+                        <div className="row justify-content-between align-items-baseline ajusted">
+                            <div className="col-6 text-start">
+                                <div style={{color: "#fefefe"}}>
+                                    Designed and developed by Bab
+                                </div>
+                                <div>
+                                    <i className="fa-regular fa-copyright"></i>
+                                    <span> 2024 All rights reserved</span>
+                                </div>
+                            </div>  
+                            
+                            <div className="col-6 text-end">
+                                <a href="mailto:chanitebrandon@gmail.com" className="bg-icone">
+                                    <i className="fa-solid fa-envelope"></i>
+                                </a>
+                                <a href="https://github.com/bab882" className="bg-icone">
+                                    <i className="fa-brands fa-github"></i>
+                                </a>
+                                <a href="https://www.linkedin.com/in/brandonchanite7a021252" className="bg-icone">
+                                    <i className="fa-brands fa-linkedin-in"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    
                 </div> 
             </div>
         </footer>
