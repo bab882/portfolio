a3b261758058639f98e2b7b05a6bf53f1a6c0700
bab882 <chanitebrandon@gmail.com>
2024-09-24 08:54:21 +0200

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
 
   return (
     
-    <footer className="container-fluid">
+    <footer className="container-fluid" id="contact">
       <div className="lighthing2"></div>
       <div className="lighthing3"></div>
       <div className="line"></div>
