6079e915f6bb2f45f4ed4226ea8ab3612bd5e658
bab882 <chanitebrandon@gmail.com>
2024-09-24 08:24:42 +0200

    maj mail

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -12,6 +12,7 @@ function Footer() {
   }, []);
 
   return (
+    
     <footer className="container-fluid">
       <div className="lighthing2"></div>
       <div className="lighthing3"></div>
@@ -22,16 +23,9 @@ function Footer() {
             <img className="logo" src={logo} alt="logo" />
           </a>
         </div>
-
         <div className="footer__title">
           <h2 className="subtitle">Interested in talking, let’s do it.</h2>
-          <a
-            className="btn btn1 justify-content-center mx-2"
-            href={`mailto:${email}`}
-            type="button"
-          >
-            Contact
-          </a>
+          <a className="btn btn1 justify-content-center mx-2" href={`mailto:${email}`} type="button" >Contact</a>
           <div className="line2 ">
             <div className="row justify-content-between align-items-baseline ajusted">
               <div className="col-12 col-md-6 text-start">
@@ -44,16 +38,13 @@ function Footer() {
                 </div>
               </div>
               <div className="col-12 col-md-6 text-end">
-                <a href="mailto:chanitebrandon@gmail.com" className="bg-icone">
+                <a href={`mailto:${email}`} className="bg-icone">
                   <i className="fa-solid fa-envelope"></i>
                 </a>
                 <a href="https://github.com/bab882" className="bg-icone">
                   <i className="fa-brands fa-github"></i>
                 </a>
-                <a
-                  href="https://www.linkedin.com/in/brandonchanite7a021252"
-                  className="bg-icone"
-                >
+                <a href="https://www.linkedin.com/in/brandonchanite7a021252" className="bg-icone" >
                   <i className="fa-brands fa-linkedin-in"></i>
                 </a>
               </div>
