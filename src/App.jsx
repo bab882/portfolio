b171e6d9ffe2e25cb2a86e15507303d814aa6f8d
bab882 <chanitebrandon@gmail.com>
2024-09-30 14:52:36 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Banner from './components/Banner';
 import NavBar from './components/NavBar';
 import Project from './components/Project';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
 import SnakeGame from './components/SnakeGame';
+
 
 function App() {
   return (
