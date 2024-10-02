dec7d364dbf14a1e454bb1ed30bdc79bf8ed38e7
bab882 <chanitebrandon@gmail.com>
2024-10-02 12:33:40 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import NotFound from './pages/NotFound';
-import ServerError from './pages/ServerError';
+// import ServerError from './pages/ServerError';
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<div className='App'><Home /></div> } />
         <Route path="/snake" element={<SnakeGame />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/500" element={<ServerError />} />
+        {/* <Route path="/500" element={<ServerError />} /> */}
       </Routes>
     </Router>
   );
