d7ad773466827d2f6d90c43d78d3f3772932aa4e
bab882 <chanitebrandon@gmail.com>
2024-10-02 12:28:49 +0200

    commit 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
+import NotFound from './pages/NotFound';
+import ServerError from './pages/ServerError';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div className='App'>
-              <Home />
-            </div>
-          }
-        /> 
-        <Route 
-          path="/snake" 
-          element={
-            <SnakeGame />
-        } />
+        <Route path="/" element={<div className='App'><Home /></div> } />
+        <Route path="/snake" element={<SnakeGame />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/500" element={<ServerError />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ServerError.jsx
@@ -0,0 +1,18 @@
+// src/pages/ServerError.js
+import React from 'react';
+
+function ServerError() {
+    return (
+        
+        <div className="container">
+        <div className="col-12 d-flex justify-content-center align-items-center vh-100">
+            <div className='className="text-center"' style={{color: '#fefefe'}}>
+                <h1>500 | Erreur Interne du Serveur</h1>
+                <Link className='btn btn1' to="/" style={{left: '35%'}}>Return</Link>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default ServerError;
