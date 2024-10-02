69823f4be21bb6ed97d79f460f36d68c57800b54
bab882 <chanitebrandon@gmail.com>
2024-10-02 09:53:21 +0200

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Banner from './components/Banner';
-import NavBar from './components/NavBar';
-// import Project from './components/Project';
-// import Portfolio from './components/Portfolio';
-import Footer from './components/Footer';
-import SnakeGame from './pages/SnakeGame';
 import Home from "./pages/Home";
+import SnakeGame from './pages/SnakeGame';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route principale avec tous les composants */}
         <Route
           path="/"
           element={
             <div className='App'>
-              {/* <Banner /> */}
               <Home />
-              
-              {/* <Project /> */}
-              {/* <Portfolio /> */}
-              {/* <Footer /> */}
             </div>
           }
         /> 
-        {/* Routes pour les projets sans rappeler les autres composants */}
         <Route 
           path="/snake" 
           element={
