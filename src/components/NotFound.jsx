97010338f4830d3e934398f4e44bd460ede1665a
bab882 <chanitebrandon@gmail.com>
2024-10-02 13:09:19 +0200

    commit

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,12 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
     return (
         <div className="container">
             <div className="col-12 d-flex justify-content-center align-items-center vh-100">
                 <div className="text-center" style={{ color: '#fefefe' }}>
-                    <h1>404 | This page could not be found.</h1>
+                    <h1 style={{ color: '#fefefe' }}>404 | This page could not be found.</h1>
                     <Link className='btn btn1' to="/" style={{ left: '35%' }}>Return</Link>
                 </div>
             </div>
