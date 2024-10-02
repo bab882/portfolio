fcad349f4e5cd33a9220fceef5839c94393535d1
bab882 <chanitebrandon@gmail.com>
2024-10-02 14:56:27 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import Error404 from './pages/Error404';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error404 />,
+  },
+  {
+    path: '/snake',
+    element: <SnakeGame />,
+  },
+  
+]);
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<div className='App'><Home /></div> } />
-        <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/*" element={<Error404 />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
+    // <Router>
+    //   <Routes>
+    //     {/* <Route path="/" element={<div className='App'><Home /></div> } /> */}
+    //     <Route path="/snake" element={<SnakeGame />} />
+    //     <Route path="/*" element={<Error404 />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Error404() {
     return (
@@ -7,7 +7,7 @@ export default function Error404() {
             <div className="col-12 d-flex justify-content-center align-items-center vh-100">
                 <div id="error-page" className="text-center" style={{ color: '#fefefe' }}>
                     <h1 style={{ color: '#fefefe' }}>404 | This page could not be found.</h1>
-                    {/* <Link className='btn btn1' to="/" style={{ left: '35%' }}>Return</Link> */}
+                    <Link className='btn btn1' to="/" style={{ left: '35%' }}>Return</Link>
                 </div>
             </div>
         </div>
