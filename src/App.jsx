d62d01c5e6dd9d11237ff36bdfee960e3474de73
bab882 <chanitebrandon@gmail.com>
2024-10-03 17:08:25 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home'; // Assure-toi que le chemin correspond à ton fichier
 import SnakeGame from './pages/SnakeGame'; // Assure-toi que le chemin correspond à ton fichier
 import NotFound from './pages/NotFound'; // Assure-toi que le chemin correspond à ton fichier
 
-const routes = [
-  { path: '/', element: <Home /> },
-  { path: '/snake', element: <SnakeGame /> },
-  { path: '/*', element: <NotFound /> },
-  // { path: '*', element: <Navigate to="/404" replace /> }
-];
+const routes = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/snake', element: <SnakeGame /> },
+    { path: '/*', element: <NotFound /> },
+    { path: '*', element: <Navigate to="/404" replace /> }
+]);
 
 function App() {
   return (
