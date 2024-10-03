dd1b34c1da6d7e6629dd294512002332f321148d
bab882 <chanitebrandon@gmail.com>
2024-10-03 11:33:41 +0200

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
+import { BrowserRouter , Routes, Route, Navigate, createBrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import NotFound from './pages/NotFound';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/snake",
-    element: <SnakeGame />,
-  },
-  {
-    path: "*",  // Route wildcard pour gérer toutes les autres routes non définies
-    element: <NotFound />,
-  }
-]);
 
 function App() {
-  return ( <RouterProvider router={router} />);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div className='App'><Home /></div> } />
+        <Route path="/snake" element={<SnakeGame />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/404" replace />} />
+      </Routes>
+    </BrowserRouter>
+ );
 }
 
 export default App;
