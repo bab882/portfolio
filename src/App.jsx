d5088d44ac8f99f627b8149716fdbcaeeec12ead
bab882 <chanitebrandon@gmail.com>
2024-10-03 09:18:19 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter , Routes, Route, Navigate } from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<div className='App'><Home /></div> } />
-        <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" replace />} />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<div className='App'><Home /></div> } />
+    //     <Route path="/snake" element={<SnakeGame />} />
+    //     <Route path="/404" element={<NotFound />} />
+    //     <Route path="*" element={<Navigate to="/404" replace />} />
+    //   </Routes>
+    // </Router>
+    <BrowserRouter> 
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<SnakeGame />} />
+
+        <Route path="/404" element={ <NotFound /> } />
+        <Route path="*" element={ <Navigate to="/404" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
