53651942f773fda75e09f53d1e52d876fd0fc116
bab882 <chanitebrandon@gmail.com>
2024-10-03 17:37:25 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter , Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router , Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import NotFound from './pages/NotFound';
@@ -7,13 +7,13 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
  );
 }
 
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import React from "react";
 import { Link } from "react-router-dom";
 
 function NotFound() {
-    const error = useRouteError();
     return (
         <div className="container">
             <div className="col-12 d-flex justify-content-center align-items-center vh-100">
                 <div id="error-page" className="text-center" style={{ color: '#fefefe' }}>
                     <h1 style={{ color: '#fefefe' }}>
-                    {error.status == "404" ? "404 Page Not Found" : ""}
-                        {/* 404 | This page could not be found. */}
+                        404 | This page could not be found.
                     </h1>
-                    {/* <Link className='btn btn1' to="/" style={{ left: '35%' }}>Return</Link> */}
+                    <Link className='btn btn1' to="/" style={{ left: '0%' }}>Return</Link>
                 </div>
             </div>
         </div>
