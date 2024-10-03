c65a8397b39ab8471d1a70739e6b564fe60dd959
bab882 <chanitebrandon@gmail.com>
2024-10-03 16:40:06 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { BrowserRouter , Routes, Route, Navigate, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter , Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
 import NotFound from './pages/NotFound';
 
 
+const Root = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/snake" element={<SnakeGame />} />
+    <Route path="/404" element={<NotFound />} />
+    <Route path="*" element={<Navigate to="/404" replace />} />
+  </Routes>
+);
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<div className='App'><Home /></div> } />
-        <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="/*" element={<Navigate to="/404" replace />} />
-      </Routes>
+      <Root />
     </BrowserRouter>
- );
+  );
 }
 
 export default App;
