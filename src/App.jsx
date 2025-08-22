9264c285d2162800ca2072c6d0a051092b1405f3
Bab Codes <chanitebrandon@gmail.com>
2025-08-22 18:39:23 +0200

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { BrowserRouter , Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./pages/Home";
 import SnakeGame from './pages/SnakeGame';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import Banner from "../components/Banner";
 import NavBar from "../components/NavBar";
 import Portfolio from "../components/Portfolio";
